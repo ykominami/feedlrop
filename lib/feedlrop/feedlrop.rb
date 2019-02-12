c06b47f6c5ed7f64ad751eb6033d35707fb1ea6b
yasuo kominami <ykominami@gmail.com>
2019-02-12 16:30:04 +0900

    fixes #70 output file to output directory

--- lib/feedlrop/feedlrop.rb
+++ lib/feedlrop/feedlrop.rb
@@ -4,10 +4,10 @@ require 'csv'
 require 'pp'
 require 'feedlr'
 require 'awesome_print'
+require 'fileutils'
 
 module Feedlrop
   class Feedlrop
-
     def initialize( token , kind, hs , opts )
       @oauth_access_token = token
       register_time = Arxutils::Dbutil::DbMgr.init( hs["db_dir"], hs["migrate_dir"] , hs["config_dir"], hs["dbconfig"] , hs["env"] , hs["log_fname"] , opts )
@@ -18,10 +18,13 @@ module Feedlrop
       @categories = @client.user_categories
     end
 
-    def get_output_file( ext )
+    # 
+    def get_output_file( parent_dir , ext )
       n = Time.now
       fname = %Q!#{n.year}-#{n.month}-#{n.day}-#{n.hour}-#{n.min}-#{n.sec}.#{ext}!
-      File.open( fname , "w")
+      FileUtils.mkdir_p( parent_dir ) if File.exists?( parent_dir )
+		  outfname = File.join( parent_dir , fname )
+      File.open( outfname , "w")
     end
 
     def get_all_unread_count
@@ -57,8 +60,8 @@ module Feedlrop
       @client.user_subscriptions.map{|m|  ap m , option }
     end
 
-    def csv_open
-      @csv = CSV.new(get_output_file("csv") , {
+    def csv_open( parent_dir )
+      @csv = CSV.new(get_output_file( parent_dir , "csv" ) , {
                        :headers => %w!category_id id count!,
                        :write_headers => true
                      } )
